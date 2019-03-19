6bd71d5c474499ca00641f9dfa033be146a8f10a
flashboss <l.stancapiano@sourcesense.com>
2019-03-19 18:17:42 +0100

    first images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Vote } from './vote/Vote.js';
 import './App.css';
 
@@ -9,7 +8,6 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
                     <Vote />
                 </header>
             </div>
--- frontend/src/vote/Vote.js
+++ frontend/src/vote/Vote.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'primereact/button';
 import { Growl } from 'primereact/growl';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './Vote.css';
 
 export class Vote extends Component {
 
@@ -21,11 +25,11 @@ export class Vote extends Component {
                     <Growl ref={(el) => this.growl = el}></Growl>
 
                     <h3>Vota</h3>
-                    <Button label="PD" icon="pi pi-plus" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
-                    <Button label="Lega" icon="pi pi-plus" onClick={this.save} className="p-button-success" style={{ marginRight: '.25em' }}></Button>
-                    <Button label="Forza Italia" icon="pi pi-plus" onClick={this.save} className="p-button-info" style={{ marginRight: '.25em' }}></Button>
-                    <Button label="Movimento 5 Stelle" icon="pi pi-plus" onClick={this.save} className="p-button-warning" style={{ marginRight: '.25em' }}></Button>
-                    <Button label="Casapound" icon="pi pi-plus" onClick={this.save} className="p-button-danger"></Button>
+                    <Button label="Forza Italia" icon="pi pi-1" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
+                    <Button label="PD" icon="pi pi-2" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
+                    <Button label="Movimento 5 Stelle" icon="pi pi-3" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
+                    <Button label="Lega" icon="pi pi-4" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
+                    <Button label="Casapound" icon="pi pi-5" onClick={this.save} className="p-button-secondary" style={{ marginRight: '.25em' }}></Button>
                 </div>
             </div>
         )
