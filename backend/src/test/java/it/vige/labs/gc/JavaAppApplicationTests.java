c7bb12cd6453c89b60edf25d1cefa9e9391d2b83
flashboss <l.stancapiano@sourcesense.com>
2019-03-30 18:16:04 +0100

    candidates

--- backend/src/test/java/it/vige/labs/gc/JavaAppApplicationTests.java
+++ backend/src/test/java/it/vige/labs/gc/JavaAppApplicationTests.java
@@ -19,7 +19,7 @@ public class JavaAppApplicationTests {
 	Logger logger = LoggerFactory.getLogger(JavaAppApplicationTests.class);
 
 	@Test
-	public void loadImages() throws IOException {
+	public void loadParties() throws IOException {
 
 		BufferedInputStream forzaItalia = (BufferedInputStream) this.getClass()
 				.getResourceAsStream("/parties/forzaitalia.png");
@@ -43,6 +43,103 @@ public class JavaAppApplicationTests {
 				.getResourceAsStream("/parties/fratelliditalia.png");
 		logger.info(new String(Base64.getEncoder().encodeToString(fratellidItalia.readAllBytes())));
 		Assert.assertNotNull(fratellidItalia);
+	}
+
+	@Test
+	public void loadCandidates() throws IOException {
+
+		BufferedInputStream candidate1 = (BufferedInputStream) this.getClass()
+				.getResourceAsStream("/candidates/candidate1.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate1.readAllBytes())));
+		Assert.assertNotNull(candidate1);
+
+		BufferedInputStream candidate2 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate2.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate2.readAllBytes())));
+		Assert.assertNotNull(candidate2);
+
+		BufferedInputStream candidate3 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate3.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate3.readAllBytes())));
+		Assert.assertNotNull(candidate3);
+
+		BufferedInputStream candidate4 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate4.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate4.readAllBytes())));
+		Assert.assertNotNull(candidate4);
+
+		BufferedInputStream candidate5 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate5.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate5.readAllBytes())));
+		Assert.assertNotNull(candidate5);
+
+		BufferedInputStream candidate6 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate6.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate6.readAllBytes())));
+		Assert.assertNotNull(candidate6);
+
+		BufferedInputStream candidate7 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate7.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate7.readAllBytes())));
+		Assert.assertNotNull(candidate7);
+
+		BufferedInputStream candidate8 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate8.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate8.readAllBytes())));
+		Assert.assertNotNull(candidate8);
+
+		BufferedInputStream candidate9 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate9.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate9.readAllBytes())));
+		Assert.assertNotNull(candidate9);
+
+		BufferedInputStream candidate10 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate10.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate10.readAllBytes())));
+		Assert.assertNotNull(candidate10);
+
+		BufferedInputStream candidate11 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate11.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate11.readAllBytes())));
+		Assert.assertNotNull(candidate11);
+
+		BufferedInputStream candidate12 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate12.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate12.readAllBytes())));
+		Assert.assertNotNull(candidate12);
+
+		BufferedInputStream candidate13 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate13.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate13.readAllBytes())));
+		Assert.assertNotNull(candidate13);
+
+		BufferedInputStream candidate14 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate14.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate14.readAllBytes())));
+		Assert.assertNotNull(candidate14);
+
+		BufferedInputStream candidate15 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate15.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate15.readAllBytes())));
+		Assert.assertNotNull(candidate15);
+
+		BufferedInputStream candidate16 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate16.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate16.readAllBytes())));
+		Assert.assertNotNull(candidate16);
+
+		BufferedInputStream candidate17 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate17.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate17.readAllBytes())));
+		Assert.assertNotNull(candidate17);
+
+		BufferedInputStream candidate18 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate18.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate18.readAllBytes())));
+		Assert.assertNotNull(candidate18);
+
+		BufferedInputStream candidate19 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate19.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate19.readAllBytes())));
+		Assert.assertNotNull(candidate19);
+
+		BufferedInputStream candidate20 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate20.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate20.readAllBytes())));
+		Assert.assertNotNull(candidate20);
+
+		BufferedInputStream candidate21 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate21.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate21.readAllBytes())));
+		Assert.assertNotNull(candidate21);
+
+		BufferedInputStream candidate22 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate22.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate22.readAllBytes())));
+		Assert.assertNotNull(candidate22);
+
+		BufferedInputStream candidate23 = (BufferedInputStream) this.getClass().getResourceAsStream("/candidates/candidate23.png");
+		logger.info(new String(Base64.getEncoder().encodeToString(candidate23.readAllBytes())));
+		Assert.assertNotNull(candidate23);
 	}
 
 }
